36e2425bac33bfb1d0507c0b542d2bd4afbffbd5
Gabe R. <9311953+marsfan@users.noreply.github.com>
2025-09-27 17:18:10 -0700

    Initial committ

---
+++ README.md
@@ -0,0 +1,6 @@
+# Various Small Python Tools
+
+This is just a collection of various small python tools that I use.
+
+Don't expect anything about this to be stable, as I will tweak it from
+time-to-time specifically for my needs.
---
+++ src/marsfan_py_tools/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https: //mozilla.org/MPL/2.0/.
+"""Package base file."""
+
+__version__ = "0.0.1"
